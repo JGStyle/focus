16b1a92d0178b72dce8672dc9c77c66054dae360
JGStyle <jgs@jws.de>
2022-02-11 19:22:03 +0100

    added dark mode + web app capable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const [progress, setProgress] = useState(0);
@@ -57,28 +58,37 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-black mb-3">{status}</h2>
-      <div className="group w-56 h-56 rounded-full hover:rounded-8xl bg-black hover:w-96 hover:h-80 flex flex-col items-center justify-center transition-all">
-        <div
-          className="h-56 group-hover:hidden transition-all"
-          style={{ transform: "rotate(" + progress + "deg)" }}
-        >
-          <div className="w-3 h-14 mt-3 rounded-full bg-white transform"></div>
-        </div>
-        <h2 className="absolute block group-hover:hidden text-white font-extrabold text-5xl">
-          {minutes}
+    <div>
+      <Head>
+        <meta name="apple-mobile-web-abb-capable" content="yes" />
+        <meta name="mobile-web-abb-capable" content="yes" />
+        <meta name="mobile-web-abb-title" content="focus" />
+      </Head>
+      <div className="h-screen flex flex-col items-center justify-center dark:bg-black">
+        <h2 className="text-3xl font-black mb-3 dark:text-slate-300">
+          {status}
         </h2>
-        <div className="flex flex-col items-center justify-center transform translate-y-16 group-hover:translate-y-0 transition-all">
-          <h1 className="hidden group-hover:block text-white text-8xl font-black text-center transition-all">
-            {minutes}:{seconds < 10 ? `0${seconds}` : `${seconds}`}
-          </h1>
-          <button
-            onClick={handleClick}
-            className="hidden group-hover:block bg-white rounded-full px-6 py-2 transition-all transform hover:scale-105"
+        <div className="group w-56 h-56 rounded-full hover:rounded-8xl bg-black dark:bg-slate-300 hover:w-96 hover:h-80 flex flex-col items-center justify-center transition-all">
+          <div
+            className="h-56 group-hover:hidden transition-all"
+            style={{ transform: "rotate(" + progress + "deg)" }}
           >
-            {timerActive ? "Pause" : "Continue"}
-          </button>
+            <div className="w-3 h-14 mt-3 rounded-full bg-white dark:bg-slate-900 transform"></div>
+          </div>
+          <h2 className="absolute block group-hover:hidden text-white dark:text-slate-900 font-extrabold text-5xl">
+            {minutes}
+          </h2>
+          <div className="flex flex-col items-center justify-center transform translate-y-16 group-hover:translate-y-0 transition-all">
+            <h1 className="hidden group-hover:block text-white dark:text-black text-8xl font-black text-center transition-all">
+              {minutes}:{seconds < 10 ? `0${seconds}` : `${seconds}`}
+            </h1>
+            <button
+              onClick={handleClick}
+              className="hidden group-hover:block bg-white dark:bg-black dark:text-white rounded-full px-6 py-2 transition-all transform hover:scale-105"
+            >
+              {timerActive ? "Pause" : "Continue"}
+            </button>
+          </div>
         </div>
       </div>
     </div>
